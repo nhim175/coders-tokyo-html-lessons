e49b2d337dfaa2900a53e6d12b8311c89e6beaa8
Thinh Pham <nhim175@gmail.com>
2018-07-23 22:44:22 +0900

    Init

---
+++ 9-js-on-browser/main.js
@@ -0,0 +1,19 @@
+function Cat() {
+  this.stomach = []
+}
+
+Cat.prototype.eat = function(mouse) {
+  this.stomach.push(mouse);
+  return this;
+};
+
+function Mouse(name) {
+  this.name = name;
+}
+
+var tom = new Cat();
+var mickey = new Mouse('Mickey');
+tom.eat(mickey);
+
+console.log(tom);
+// Chrome Developer Tools
